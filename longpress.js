4f8dad322d08cb861e52a1c3ec0bf56475af3443
Lucas Kirsche <54104257+lukirs95@users.noreply.github.com>
2021-06-04 16:03:55 +0200

    initialize everything automatically, take style from parent

--- longpress.js
+++ longpress.js
@@ -1,24 +1,18 @@
-export default class LongPressButton {
-  /**
-   * 
-   * @param {string} label 
-   * @param {function} fn 
-   * @param {number} timeout 
-   * @param {DOMTokenList} classList 
-   * @returns HtmlButtonElement
-   */
-    constructor(label = 'OK', fn = () => {}, timeout = 2000, classList) {
-      this._button = document.createElement('button');
-      this._button.classList = classList;
+class LongPressButton {
+    constructor(button) {
+      this._button = button;
+      this._button.className = 'longpress';
       this._loader = document.createElement('div');
       this._loader.className = 'loader';
       this._label = document.createElement('span');
-      this._label.textContent = label;
-      
-      this._fn = fn;
-      
+      this._label.textContent = this._button.textContent;
+      this._button.textContent = '';
+
+      this._fn = button.onclick;
+      this._button.onclick = () => {};
+
       this._touch = false;
-      this._timeout = timeout;
+      this._timeout = this._button.dataset.duration;
       this._loader.style.animationDuration = `${this._timeout}ms`;
       this._timer = undefined;
     
@@ -51,7 +45,6 @@ export default class LongPressButton {
           this._loader.classList.remove('loading');
           clearTimeout(this._timer);
           this._touch = false;
-          console.log('press End');
         }
       }, true);
   
@@ -60,20 +53,43 @@ export default class LongPressButton {
       return this._button;
     }
 
-/**
- * #private Method
- */
     _onAnimationEnd() {
-      this._button.classList.remove('loaded');
-      this._button.removeEventListener('animationend', this._onAnimationEnd.bind(this));
+      this._loader.classList.remove('loaded');
+      this._loader.removeEventListener('animationend', this._onAnimationEnd.bind(this));
     }
   
     _exec(event) {
       this._touch = false;
       this._loader.classList.remove('loading');
-      this._button.classList.add('loaded');
-      this._button.addEventListener('animationend', () => this._onAnimationEnd());
+      this._loader.classList.add('loaded');
+      this._loader.addEventListener('animationend', () => this._onAnimationEnd());
       this._fn(event);
-      // this._button.dispatchEvent(new MouseEvent('longpress', event));
     }
   }
+
+/**
+ * @param {HTMLElement} searchArea
+ */
+const init = (searchArea = undefined) => {
+  const buttons = searchArea ? searchArea.querySelectorAll('button') : document.querySelectorAll('button');
+  buttons.forEach((button) => {
+    if (button.dataset.longpress) new LongPressButton(button);
+  });
+  return buttons;
+}
+
+export default {
+  /**
+   * Create new LongPressButton and return the Button itself
+   * 
+   * @param {HTMLButtonElement} button
+   * @returns HtmlButtonElement
+   */
+  newButton: (button) => new LongPressButton(button),
+/**
+ * Initialize document and create LongPressButtons.
+ * 
+ * @param {HTMLElement} searchArea
+ */
+  init
+}
