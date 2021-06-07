250312b176d061f25a78e77485729b7b20f88ce3
Lucas Kirsche <54104257+lukirs95@users.noreply.github.com>
2021-06-07 23:02:22 +0200

    fix example... auto init

--- example/index.js
+++ example/index.js
@@ -1,10 +1,12 @@
-import { lpButton as longpress } from '../longpressbutton.js';
+import { lpButton as longpress, lpButton } from '../longpressbutton.js';
 
 const element = document.createElement('div');
 element.textContent = 'Press 2s';
 element.onclick = () => {
   alert('third button 2s');
 };
+
+lpButton.init();
 
 const customButton = longpress.newButton(element, 2000);
 
